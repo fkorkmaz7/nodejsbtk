f01d3d61b90d7eff5b818e3b1ef53df99e1526b7
furkan <furkan@furkan-MacBook-Air.local>
2024-10-06 17:26:51 +0300

    musteri router ve model eklendi

--- 4.gün/routes/musteri.js
+++ 4.gün/routes/musteri.js
@@ -3,8 +3,27 @@ const Musteri = require('../models/musteri');
 const router = express.Router();
 
 router.get('/getir',async(req,res,next) => {
-    const musteriler = Musteri.find();
+    const musteriler = await Musteri.find();
     res.send(musteriler);
 });
+
+router.post('/ekle',async(req,res,next) => {
+    const yeni_musteri = await Musteri.create(req.body);
+    res.send(yeni_musteri);
+})
+
+router.delete('/sil',async(req,res,next) => {
+    const body = req.body;
+    try{
+    const silinecek_musteri = await Musteri.findOneAndDelete({musteriTc:body.musteriTc},{new:true});
+    if(!silinecek_musteri){
+        res.status(404).send('musteri bulunamadı');
+    }
+    res.send(silinecek_musteri);
+    }
+    catch(error){
+        res.status(500).send(`müşteri silinirken hata oluştu : ${error.message}`);
+    }
+})
 
 module.exports = router;
